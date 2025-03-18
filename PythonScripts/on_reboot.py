d9f6f515c005e9234fcdcecab278743b4441fc3a
Frédéric Jacques <gary.bob88@gmail.com>
2025-03-18 18:14:07 -0400

    20 tentatives pour attendre la reprise de la connexion internet

--- PythonScripts/on_reboot.py
+++ PythonScripts/on_reboot.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from proxy.erabliere_api_proxy import ErabliereApiProxy
 
 authType = None
@@ -8,6 +9,22 @@ if len(sys.argv) > 2:
 
 proxy = ErabliereApiProxy(sys.argv[1], authType)
 
-r = proxy.envoyer_donnee_capteur(sys.argv[len(sys.argv)-1], 1)
+tentative = 0
+r = None
+while tentative < 20:
+  try:
+    r = proxy.envoyer_donnee_capteur(sys.argv[len(sys.argv)-1], 1)
+    if r.status_code != 200 and r.status_code != 201 and r.status_code != 204:
+      print(r.status_code)
+      print(r.text)
+      tentative += 1
+    else:
+      break
+  except Exception as e:
+    tentative += 1
+    print(e)
+
+  # before retrying, wait 60 seconds
+  time.sleep(60)
 
 print(r)
