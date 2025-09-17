5161ce36d4e2e2d8fff36da5d5e7ce955bcc59dd
Frédéric Jacques <gary.bob88@gmail.com>
2025-09-17 19:51:02 -0400

    remove unused function

--- ErabliereIU/cypress/integration/notes.spec.ts
+++ ErabliereIU/cypress/integration/notes.spec.ts
@@ -58,27 +58,8 @@ describe("Notes page", { testIsolation: false }, () => {
 
         // validate that note is added
         notesPage.getNoteTitle().should('have.text', title);
-        notesPage.getNoteDescription().should('have.text', content);
+        notesPage.getNoteDescription().should('have.text', `${content}\n`);
     });
-
-    function toISO8601(date: Date): string {
-        // format a date using the iso 8601 standard
-        // the date should have the offset in the format +00:00
-        // the date should be in the format YYYY-MM-DD
-        // the date should be in the format HH:mm:ss
-
-        const offset = date.getTimezoneOffset();
-        const hours = Math.floor(offset / 60);
-        const minutes = offset % 60;
-
-        const offsetString = `${hours <= 0 ? "+" : "-"}${pad2(Math.abs(hours))}:${minutes < 10 ? "0" : ""}${minutes}`;
-
-        // format the date, mounth, date, hours, minutes and seconds should be 2 digits
-        const dateString = `${date.getFullYear()}-${pad2(date.getMonth() + 1)}-${pad2(date.getDate())}`;
-        const timeString = `${pad2(date.getHours())}:${pad2(date.getMinutes())}:${pad2(date.getSeconds())}`;
-
-        return `${dateString}T${timeString}${offsetString}`;
-    }
 
     function pad2(number: number) {
         return (number < 10 ? '0' : '') + number;
