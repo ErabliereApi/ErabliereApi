67cfd22d34f2f773f285eaa3fa4c306f6c590331
Frédéric Jacques <gary.bob88@gmail.com>
2025-10-13 13:20:19 -0400

    ajustement des modeles UI

---
+++ ErabliereIU/src/app/ai-view/ai-images/ai-images.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-ai-images',
+    template: `<p>La génération d'image arrive bientôt! Déjà disponible via l'API /ErabliereAI/Images.</p>`,
+})
+export class AiImagesComponent { }
--- ErabliereIU/src/model/conversation.ts
+++ ErabliereIU/src/model/conversation.ts
@@ -15,10 +15,21 @@ export class Message {
     content?: string;
     createdAt?: Date;
     isUser?: boolean;
+    refusal?: string;
+    imageUri?: string;
+    messageParts?: MessagePart[];
 }
 
 export class PromptResponse {
     prompt: any;
     conversation?: Conversation;
     response?: any;
+}
+
+export class MessagePart {
+    id: any;
+    messageId?: any;
+    content?: string;
+    contentByte?: number[];
+    contentType?: string;
 }
